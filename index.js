151929152551f5315e8fc26b3f01762c1f255c29
noven temer <noven5823@gmail.com>
2018-09-24 10:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(name){
+	for(let i = 0; i < name.length; i++){
+		console.log(`Welcome ${name[i]}! You are employee #${i+1}.`);
+	}
+  return name;
+
+}
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
